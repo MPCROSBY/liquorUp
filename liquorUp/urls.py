84a53f200d4ca94c3ccde579c3dc42395c4afc71
Mike <mpcrosby@outlook.com>
2019-05-24 21:46:05 -0600

    Fintrack
    learned how to make an app
    
    next time, models?

--- liquorUp/urls.py
+++ liquorUp/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from liquoredUp import views
 
 urlpatterns = [
     path('', views.scrapper, name='scrapper'),
     path('admin/', admin.site.urls),
+    path('fintrack/', include('fintrack.urls')),
 ]
